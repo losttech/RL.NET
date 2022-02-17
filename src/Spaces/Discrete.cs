3995c6f285338dbf4312e691a8a1e27fe82a77b1
Victor Nova <lost@losttech.software>
2022-02-17 10:29:50 -0800

    added Discrete space

---
+++ src/Spaces/Discrete.cs
@@ -0,0 +1,14 @@
+﻿namespace RL.Spaces; 
+
+[PublicAPI]
+public struct Discrete: ISpace<Int64> {
+    public Int64 StateCount { get; }
+    public Int64 Base { get; }
+
+    public Discrete(Int64 stateCount, Int64 @base = 0) {
+        if (stateCount <= 0)
+            throw new ArgumentOutOfRangeException(nameof(stateCount), "Must be positive");
+        this.StateCount = stateCount;
+        this.Base = @base;
+    }
+}
